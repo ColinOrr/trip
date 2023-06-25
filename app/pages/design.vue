9359deb0c8157099191f884e2beb0807a8783003
Colin Orr <colin.orr@gmail.com>
2023-06-25 16:42:34 +0100

    Add a fixed header for the design

--- app/pages/design.vue
+++ app/pages/design.vue
@@ -1,17 +1,22 @@
 <template>
-  <v-container>
-    <header>
+  <header class="sticky top-0 z-10 border-b bg-white/70 backdrop-blur-sm">
+    <v-container>
       <h1>Design system</h1>
-      <a href="#icons"> Icons </a>
-      &middot;
-      <a href="#v-container"> v-container </a>
-      &middot;
-      <a href="#v-sheet"> v-sheet </a>
-      &middot;
-      <a href="#v-text-field"> v-text-field </a>
-    </header>
+      <nav class="overflow-x-auto whitespace-nowrap">
+        <a href="#icons"> Icons </a>
+        &middot;
+        <a href="#v-container"> v-container </a>
+        &middot;
+        <a href="#v-sheet"> v-sheet </a>
+        &middot;
+        <a href="#v-text-field"> v-text-field </a>
+      </nav>
+    </v-container>
+  </header>
 
-    <section id="icons">
+  <v-container class="pb-32">
+    <section>
+      <a class="anchor" id="icons" />
       <h2>Icons</h2>
       <v-sheet class="flex gap-1 text-indigo-800">
         <i-ic-outline-mail class="h-8 w-8" />
@@ -20,7 +25,8 @@
       </v-sheet>
     </section>
 
-    <section id="v-container">
+    <section>
+      <a class="anchor" id="v-container" />
       <h2>v-container</h2>
       <v-sheet>
         <v-container class="max-w-xs rounded-lg border bg-gray-100">
@@ -29,12 +35,14 @@
       </v-sheet>
     </section>
 
-    <section id="v-sheet">
+    <section>
+      <a class="anchor" id="v-sheet" />
       <h2>v-sheet</h2>
       <v-sheet> A paper-like surface for laying out UI components. </v-sheet>
     </section>
 
-    <section id="v-text-field">
+    <section>
+      <a class="anchor" id="v-text-field" />
       <h2>v-text-field</h2>
       <v-sheet class="flex flex-col gap-4">
         <v-text-field placeholder="Email" type="email">
@@ -54,6 +62,11 @@ a {
   @apply text-sm font-medium text-indigo-600 hover:text-indigo-500;
 }
 
+a.anchor {
+  position: relative;
+  top: -100px;
+}
+
 h1 {
   @apply text-xl tracking-tight text-gray-800;
 }
@@ -62,7 +75,7 @@ h2 {
   @apply font-medium tracking-tight text-gray-700;
 }
 
-section {
+section + section {
   @apply mt-8;
 }
 
